891ef188ace2e1c67acb17bd65a51774d53e2254
yassine batrach <19726@studenti.marconiverona.edu.it>
2025-04-23 10:23:58 +0200

    dsad

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,42 @@ import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, "dist/public/index.html"),
+          dest: ".", // lo mette nella stessa cartella
+          rename: "404.html"
+        },
+      ],
+      watch: {}, // per evitare warning
+    }),
     ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined 
+    process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
 });
