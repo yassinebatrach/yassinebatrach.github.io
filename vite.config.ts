d9ffc260108b385b0a3d9f83505f89a9fa428ba4
yassine batrach <19726@studenti.marconiverona.edu.it>
2025-04-23 10:19:20 +0200

    modifiche base

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     runtimeErrorOverlay(),
     themePlugin(),
     ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    process.env.REPL_ID !== undefined 
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
             m.cartographer(),
